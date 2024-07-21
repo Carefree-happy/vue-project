d63b392e18cb82ba4936ae92fa176c666c27a476
sunjing <beiyouok@dtstack.com>
2024-07-22 03:14:00 +0800

    feat: 插件的使用

--- src/main.js
+++ src/main.js
@@ -1,31 +1,16 @@
-/* 
-	该文件是整个项目的入口文件
-*/
 //引入Vue
 import Vue from 'vue'
-//引入App组件，它是所有组件的父组件
+//引入App
 import App from './App.vue'
-//关闭vue的生产提示
+//引入插件
+import plugins from './plugins'
+//关闭Vue的生产提示
 Vue.config.productionTip = false
 
-import {hunhe,hunhe2} from './mixin'
-Vue.mixin(hunhe)
-Vue.mixin(hunhe2)
-
-/* 
-	关于不同版本的Vue：
-	
-		1.vue.js与vue.runtime.xxx.js的区别：
-				(1).vue.js是完整版的Vue，包含：核心功能+模板解析器。
-				(2).vue.runtime.xxx.js是运行版的Vue，只包含：核心功能；没有模板解析器。
-
-		2.因为vue.runtime.xxx.js没有模板解析器，所以不能使用template配置项，需要使用
-			render函数接收到的createElement函数去指定具体内容。
-*/
-
-//创建Vue实例对象---vm
+//应用（使用）插件
+Vue.use(plugins,1,2,3)
+//创建vm
 new Vue({
-  //render函数完成了这个功能：将App组件放入容器中
-  render: h => h(App),
-  
-}).$mount('#app')
+	el:'#app',
+	render: h => h(App)
+})
