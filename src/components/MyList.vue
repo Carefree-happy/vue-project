f00f1e59c50c81ac716ceed209f853170b218941
sunjing <beiyouok@dtstack.com>
2024-07-25 00:38:18 +0800

    feat: todoList添加动画

--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -1,10 +1,12 @@
 <template>
 	<ul class="todo-main">
-		<MyItem 
-			v-for="todoObj in todos"
-			:key="todoObj.id" 
-			:todo="todoObj" 
-		/>
+		<transition-group name="todo" appear>
+			<MyItem 
+				v-for="todoObj in todos"
+				:key="todoObj.id" 
+				:todo="todoObj" 
+			/>
+		</transition-group>
 	</ul>
 </template>
 
@@ -38,5 +40,22 @@
 		border-radius: 2px;
 		padding-left: 5px;
 		margin-top: 10px;
+	}
+
+	.todo-enter-active{
+		animation: atguigu 0.5s linear;
+	}
+
+	.todo-leave-active{
+		animation: atguigu 0.5s linear reverse;
+	}
+
+	@keyframes atguigu {
+		from{
+			transform: translateX(100%);
+		}
+		to{
+			transform: translateX(0px);
+		}
 	}
 </style>
