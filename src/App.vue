4815149a5322146f2388c1857dc0cd0513e154de
sunjing <beiyouok@dtstack.com>
2024-07-24 20:46:37 +0800

    feat: 组件自定义事件

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,68 @@
 <template>
-	<div id="root">
-		<div class="todo-container">
-			<div class="todo-wrap">
-				<MyHeader :addTodo="addTodo"/>
-				<MyList :todos="todos" :checkTodo="checkTodo" :delTodo="delTodo"/>
-				<MyFooter :todos="todos" :clearAllTodo="clearAllTodo" :checkAllTodo="checkAllTodo"/>
-			</div>
-		</div>
+	<div class="app">
+		<h1>{{msg}}，学生姓名是:{{studentName}}</h1>
+
+		<!-- 通过父组件给子组件传递函数类型的props实现：子给父传递数据 -->
+		<School :getSchoolName="getSchoolName"/>
+
+		<!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第一种写法，使用@或v-on） -->
+		<!-- <Student @atguigu="getStudentName" @demo="m1"/> -->
+
+		<!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第二种写法，使用ref） -->
+		<Student ref="student" @click.native="show"/>
 	</div>
 </template>
 
 <script>
-	import MyHeader from './components/MyHeader'
-	import MyList from './components/MyList'
-	import MyFooter from './components/MyFooter.vue'
+	import Student from './components/Student'
+	import School from './components/School'
 
 	export default {
 		name:'App',
-		components:{MyHeader,MyList,MyFooter},
+		components:{School,Student},
 		data() {
 			return {
-				//由于todos是MyHeader组件和MyFooter组件都在使用，所以放在App中（状态提升）
-				todos:JSON.parse(localStorage.getItem('todos')) || []
-			}
-		},
-		watch: {
-			todos:{
-				deep:true,
-				handler(value){
-					localStorage.setItem('todos',JSON.stringify(value))
-				}
+				msg:'你好啊！',
+				studentName:''
 			}
 		},
 		methods: {
-			// 添加一个todo
-			addTodo(x) {
-				this.todos.unshift(x);
+			getSchoolName(name){
+				console.log('App收到了学校名：',name)
 			},
-			checkTodo(id) {
-				this.todos.forEach((todo) => {
-					if (todo.id === id) todo.done = !todo.done
-				})
+			getStudentName(name,...params){
+				console.log('App收到了学生名：',name,params)
+				this.studentName = name
 			},
-			// 删除一个todo
-			delTodo(x) {
-				this.todos = this.todos.filter((e) => {
-					return e.id !== x
-				})
+			m1(){
+				console.log('demo事件被触发了！')
 			},
-			// 清除所有已经完成的todo
-			clearAllTodo() {
-				this.todos = this.todos.filter((todo)=>{
-					return !todo.done
-				})
-			},
-			// 全选or取消全选
-			checkAllTodo(done){
-				this.todos.forEach((todo)=>{
-					todo.done = done
-				})
-			},
-		}
+			show(){
+				alert(123)
+			}
+		},
+		mounted() {
+			/**
+			 * 子组件修改父组件的值
+			 * 1、methods中定义好更改的方法，this.getStudentName调用
+			 * 2、methods中定义好更改的方法，箭头调用，没有this会去取外层方法的this
+			 * 3、如果直接是Function，则不能修改，组件调用会将自己的this赋给Function
+			 */
+			// this.$refs.student.$on('atguigu',this.getStudentName) //绑定自定义事件
+
+			this.$refs.student.$on('atguigu',(name,...params) => {
+				console.log('App收到了学生名：',name,params)
+				this.studentName = name
+			})
+
+			// this.$refs.student.$once('atguigu',this.getStudentName) //绑定自定义事件（一次性）
+		},
 	}
 </script>
 
-<style>
-	/*base*/
-	body {
-		background: #fff;
-	}
-	.btn {
-		display: inline-block;
-		padding: 4px 12px;
-		margin-bottom: 0;
-		font-size: 14px;
-		line-height: 20px;
-		text-align: center;
-		vertical-align: middle;
-		cursor: pointer;
-		box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-		border-radius: 4px;
-	}
-	.btn-danger {
-		color: #fff;
-		background-color: #da4f49;
-		border: 1px solid #bd362f;
-	}
-	.btn-danger:hover {
-		color: #fff;
-		background-color: #bd362f;
-	}
-	.btn:focus {
-		outline: none;
-	}
-	.todo-container {
-		width: 600px;
-		margin: 0 auto;
-	}
-	.todo-container .todo-wrap {
-		padding: 10px;
-		border: 1px solid #ddd;
-		border-radius: 5px;
+<style scoped>
+	.app{
+		background-color: gray;
+		padding: 5px;
 	}
 </style>
