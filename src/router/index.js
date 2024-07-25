68321730333237624f3f019a10a765a9df41008f
sunjing <beiyouok@dtstack.com>
2024-07-25 22:43:53 +0800

    feat: 独享路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,19 @@ const router = new VueRouter({
 				{
 					path:'news',
 					component:News,
-					meta:{isAuth:true,title:'新闻'}
+					meta:{isAuth:true,title:'新闻'},
+					beforeEnter: (to, from, next) => {
+						console.log('独享路由守卫',to,from)
+						if(to.meta.isAuth){ //判断是否需要鉴权
+							if(localStorage.getItem('school')==='atguigu'){
+								next()
+							}else{
+								alert('学校名不对，无权限查看！')
+							}
+						}else{
+							next()
+						}
+					}
 				},
 				{
 					path:'message',
@@ -42,33 +54,33 @@ const router = new VueRouter({
 })
 
 //全局前置路由守卫————初始化的时候被调用、每次路由切换之前被调用
-router.beforeEach((to,from,next)=>{
-	console.log('前置路由守卫',to,from,next)
-	// if(to.path === '/home/news' || to.path === '/home/message'){ //判断是否需要鉴权
-	// 	if(localStorage.getItem('school')==='atguigu'){
-	// 		next()
-	// 	}else{
-	// 		alert('学校名不对，无权限查看！')
-	// 	}
-	// }else{
-	// 		next()
-	// }
+// router.beforeEach((to,from,next)=>{
+// 	console.log('前置路由守卫',to,from,next)
+// 	// if(to.path === '/home/news' || to.path === '/home/message'){ //判断是否需要鉴权
+// 	// 	if(localStorage.getItem('school')==='atguigu'){
+// 	// 		next()
+// 	// 	}else{
+// 	// 		alert('学校名不对，无权限查看！')
+// 	// 	}
+// 	// }else{
+// 	// 		next()
+// 	// }
 
-	if(to.meta.isAuth){ //判断是否需要鉴权
-		if(localStorage.getItem('school')==='atguigu'){
-			next()
-		}else{
-			alert('学校名不对，无权限查看！')
-		}
-	}else{
-		next()
-	}
-})
+// 	if(to.meta.isAuth){ //判断是否需要鉴权
+// 		if(localStorage.getItem('school')==='atguigu'){
+// 			next()
+// 		}else{
+// 			alert('学校名不对，无权限查看！')
+// 		}
+// 	}else{
+// 		next()
+// 	}
+// })
 
 //全局后置路由守卫————初始化的时候被调用、每次路由切换之后被调用
-router.afterEach((to,from)=>{
-	console.log('后置路由守卫',to,from)
-	document.title = to.meta.title || '硅谷系统'
-})
+// router.afterEach((to,from)=>{
+// 	console.log('后置路由守卫',to,from)
+// 	document.title = to.meta.title || '硅谷系统'
+// })
 
 export default router
