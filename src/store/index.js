9c5c49e6405dc26625ba84dba20a5e9176d1f649
sunjing <beiyouok@dtstack.com>
2024-07-25 17:12:32 +0800

    feat: mapState和mapGetters

--- src/store/index.js
+++ src/store/index.js
@@ -41,7 +41,9 @@ const mutations = {
 }
 //准备state——用于存储数据
 const state = {
-	sum:0 //当前的和
+	sum:0, //当前的和
+	school:'尚硅谷',
+	subject:'前端'
 }
 //准备getters——用于将state中的数据进行加工
 const getters = {
@@ -49,10 +51,11 @@ const getters = {
 		return state.sum*10
 	}
 }
+
 //创建并暴露store
 export default new Vuex.Store({
 	actions,
 	mutations,
 	state,
-	getters,
+	getters
 })
