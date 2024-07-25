cae2d45f28e55a78720c950624cdb1c520ed4df2
sunjing <beiyouok@dtstack.com>
2024-07-25 22:41:56 +0800

    feat:路径鉴权配置

--- src/router/index.js
+++ src/router/index.js
@@ -22,10 +22,12 @@ const router = new VueRouter({
 				{
 					path:'news',
 					component:News,
+					meta:{isAuth:true,title:'新闻'}
 				},
 				{
 					path:'message',
 					component:Message,
+					meta:{isAuth:true,title:'消息'},
 					children:[
 						{
 							name:'xiangqing',
@@ -42,25 +44,25 @@ const router = new VueRouter({
 //全局前置路由守卫————初始化的时候被调用、每次路由切换之前被调用
 router.beforeEach((to,from,next)=>{
 	console.log('前置路由守卫',to,from,next)
-	if(to.path === '/home/news' || to.path === '/home/message'){ //判断是否需要鉴权
-		if(localStorage.getItem('school')==='atguigu'){
-			next()
-		}else{
-			alert('学校名不对，无权限查看！')
-		}
-	}else{
-			next()
-	}
-
-	// if(to.meta.isAuth){ //判断是否需要鉴权
+	// if(to.path === '/home/news' || to.path === '/home/message'){ //判断是否需要鉴权
 	// 	if(localStorage.getItem('school')==='atguigu'){
 	// 		next()
 	// 	}else{
 	// 		alert('学校名不对，无权限查看！')
 	// 	}
 	// }else{
-	// 	next()
+	// 		next()
 	// }
+
+	if(to.meta.isAuth){ //判断是否需要鉴权
+		if(localStorage.getItem('school')==='atguigu'){
+			next()
+		}else{
+			alert('学校名不对，无权限查看！')
+		}
+	}else{
+		next()
+	}
 })
 
 //全局后置路由守卫————初始化的时候被调用、每次路由切换之后被调用
