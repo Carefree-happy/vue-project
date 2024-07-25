3db335a0e6256fe2e79f6a659ab0dbf8446fd8ca
sunjing <beiyouok@dtstack.com>
2024-07-25 21:57:52 +0800

    feat: 激活与失活组件

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -12,5 +12,17 @@
 		beforeDestroy() {
 			console.log('News组件即将被销毁了')
 		},
+		activated() {
+			console.log('News组件被激活了')
+			this.timer = setInterval(() => {
+				console.log('@')
+				this.opacity -= 0.01
+				if(this.opacity <= 0) this.opacity = 1
+			},16)
+		},
+		deactivated() {
+			console.log('News组件失活了')
+			clearInterval(this.timer)
+		},
 	}
 </script>
