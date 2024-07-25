54944faef8afbd2165d6fb6b19eab859bb4f6ac7
sunjing <beiyouok@dtstack.com>
2024-07-25 20:19:36 +0800

    feat: 路由的query参数

--- src/pages/Message.vue
+++ src/pages/Message.vue
@@ -1,14 +1,21 @@
 <template>
 	<div>
 		<ul>
-			<li>
-				<a href="/message1">message001</a>&nbsp;&nbsp;
-			</li>
-			<li>
-				<a href="/message2">message002</a>&nbsp;&nbsp;
-			</li>
-			<li>
-				<a href="/message/3">message003</a>&nbsp;&nbsp;
+			<li v-for="m in messageList" :key="m.id">
+				<!-- 跳转路由并携带query参数，to的字符串写法 -->
+				<!-- <router-link :to="`/home/message/detail?id=${m.id}&title=${m.title}`">{{m.title}}</router-link>&nbsp;&nbsp; -->
+
+				<!-- 跳转路由并携带query参数，to的对象写法 -->
+				<router-link :to="{
+					path:'/home/message/detail',
+					query:{
+						id:m.id,
+						title:m.title
+					}
+				}">
+					{{m.title}}
+				</router-link>
+			
 			</li>
 		</ul>
 	</div>
@@ -16,6 +23,15 @@
 
 <script>
 	export default {
-		name:'MessageInfo'
+		name:'MessageInfo',
+		data() {
+			return {
+				messageList:[
+					{id:'001',title:'消息001'},
+					{id:'002',title:'消息002'},
+					{id:'003',title:'消息003'}
+				]
+			}
+		},
 	}
 </script>
