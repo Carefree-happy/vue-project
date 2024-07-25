6ed4e403a43291d9c006b3c97b0b095bb1bdfc15
sunjing <beiyouok@dtstack.com>
2024-07-25 16:37:17 +0800

    feat: 求和案例

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,16 @@
 <template>
-	<div class="container">
-
-		<Category title="游戏">
-			<template scope="atguigu">
-				<ul>
-					<li v-for="(g,index) in atguigu.games" :key="index">{{g}}</li>
-				</ul>
-			</template>
-		</Category>
-
-		<Category title="游戏">
-			<template scope="{games}">
-				<ol>
-					<li style="color:red" v-for="(g,index) in games" :key="index">{{g}}</li>
-				</ol>
-			</template>
-		</Category>
-
-		<Category title="游戏">
-			<template slot-scope="{games}">
-				<h4 v-for="(g,index) in games" :key="index">{{g}}</h4>
-			</template>
-		</Category>
-
+	<div>
+		<Count/>
 	</div>
 </template>
 
 <script>
-	import Category from './components/Category'
+	import Count from './components/Count'
 	export default {
 		name:'App',
-		components:{Category},
+		components:{Count},
+		mounted() {
+			// console.log('App',this)
+		},
 	}
 </script>
-
-<style scoped>
-	.container,.foot{
-		display: flex;
-		justify-content: space-around;
-	}
-	h4{
-		text-align: center;
-	}
-</style>
