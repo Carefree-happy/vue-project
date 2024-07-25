3b64c5256d35ca71bbeb8a77ecb0e32bd8e3c89d
sunjing <beiyouok@dtstack.com>
2024-07-25 22:33:57 +0800

    feat: 根据路径判断是否鉴权

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -1,5 +1,6 @@
 <template>
 	<ul>
+		<li :style="{opacity}">欢迎学习Vue</li>
 		<li>news001 <input type="text"></li>
 		<li>news002 <input type="text"></li>
 		<li>news003 <input type="text"></li>
@@ -9,6 +10,11 @@
 <script>
 	export default {
 		name:'NewsInfo',
+		data() {
+			return {
+				opacity:1
+			}
+		},
 		beforeDestroy() {
 			console.log('News组件即将被销毁了')
 		},
