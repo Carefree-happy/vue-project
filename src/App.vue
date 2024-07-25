681b0f76f0ba39a3e24865905659db0a38a4b8ca
sunjing <beiyouok@dtstack.com>
2024-07-26 01:33:41 +0800

    feat: ref 处理对象

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,41 @@
   <h1 @click="sayHello">我是APP组件</h1>
   <h2>姓名：{{ name }}</h2>
   <h2>年龄：{{ age }}</h2>
+  <h3>工作种类：{{job.type}}</h3>
+	<h3>工作薪水：{{job.salary}}</h3>
+  <button @click="changeInfo">修改信息</button>
 </template>
 <script>
+import { ref } from 'vue'
   export default {
     name: 'APP',
     setup() {
       // 数据
-      let name = '张三'
-      let age = 18
+      let name = ref('张三')
+      let age = ref(18)
+      let job = ref({
+				type:'前端工程师',
+				salary:'30K'
+			})
+
+      function changeInfo() {
+        name.value = 'lisi'
+        age.value = 28
+        job.value.type = 'UI设计师'
+				job.value.salary = '60K'
+      }
 
       // 方法
       function sayHello() {
-        alert(`我叫${name},我今年${age}岁了，你好呀！`)
+        alert(`我叫${name.value},我今年${age.value}岁了，你好呀！`)
       }
 
       return {
         name,
         age,
-        sayHello
+        job,
+        sayHello,
+        changeInfo
       }
     }
   }
