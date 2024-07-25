d43355bba5a5e701e9eb565049869a9d2a5c8ec3
sunjing <beiyouok@dtstack.com>
2024-07-25 17:27:07 +0800

    feat: mapMutations和mapActions

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -8,15 +8,15 @@
 			<option value="2">2</option>
 			<option value="3">3</option>
 		</select>
-		<button @click="increment">+</button>
-		<button @click="decrement">-</button>
-		<button @click="incrementOdd">当前求和为奇数再加</button>
-		<button @click="incrementWait">等一等再加</button>
+		<button @click="increment(n)">+</button>
+		<button @click="decrement(n)">-</button>
+		<button @click="incrementOdd(n)">当前求和为奇数再加</button>
+		<button @click="incrementWait(n)">等一等再加</button>
 	</div>
 </template>
 
 <script>
-	import {mapState,mapGetters} from 'vuex'
+	import {mapState,mapGetters,mapMutations,mapActions} from 'vuex'
 	export default {
 		name:'CountInfo',
 		data() {
@@ -56,18 +56,35 @@
 
 		},
 		methods: {
-			increment(){
+			//程序员亲自写方法
+			/* increment(){
 				this.$store.commit('JIA',this.n)
 			},
 			decrement(){
 				this.$store.commit('JIAN',this.n)
-			},
-			incrementOdd(){
+			}, */
+
+			//借助mapMutations生成对应的方法，方法中会调用commit去联系mutations(对象写法)
+			...mapMutations({increment:'JIA',decrement:'JIAN'}),
+
+			//借助mapMutations生成对应的方法，方法中会调用commit去联系mutations(数组写法)
+			// ...mapMutations(['JIA','JIAN']),
+
+			/* ************************************************* */
+
+			//程序员亲自写方法
+			/* incrementOdd(){
 				this.$store.dispatch('jiaOdd',this.n)
 			},
 			incrementWait(){
 				this.$store.dispatch('jiaWait',this.n)
-			},
+			}, */
+
+			//借助mapActions生成对应的方法，方法中会调用dispatch去联系actions(对象写法)
+			...mapActions({incrementOdd:'jiaOdd',incrementWait:'jiaWait'})
+
+			//借助mapActions生成对应的方法，方法中会调用dispatch去联系actions(数组写法)
+			// ...mapActions(['jiaOdd','jiaWait'])
 		},
 		mounted() {
 			const x = mapState({he:'sum',xuexiao:'school',xueke:'subject'})
