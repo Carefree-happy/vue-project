9476cfa9f92965d0be4ebc9b4a7ebb5474a75320
sunjing <beiyouok@dtstack.com>
2024-07-26 15:50:47 +0800

    feat: 响应式数据判断

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-	<div class="app">
-		<h3>我是App组件（祖），{{name}}--{{price}}</h3>
-		<Child/>
-	</div>
+	<h3>我是App组件</h3>
 </template>
 
 <script>
-	import { reactive,toRefs,provide } from 'vue'
-	import Child from './components/Child.vue'
+	import {ref, reactive,toRefs,readonly,isRef,isReactive,isReadonly,isProxy } from 'vue'
 	export default {
 		name:'App',
-		components:{Child},
 		setup(){
 			let car = reactive({name:'奔驰',price:'40W'})
-			provide('car',car) //给自己的后代组件传递数据
+			let sum = ref(0)
+			let car2 = readonly(car)
+
+			console.log(isRef(sum))
+			console.log(isReactive(car))
+			console.log(isReadonly(car2))
+			console.log(isProxy(car))
+			console.log(isProxy(sum))
+
+			
 			return {...toRefs(car)}
 		}
 	}
